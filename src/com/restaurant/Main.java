fc2b1f60ccd635f960645a012eb154e63666868b
Mry3zof <yousseffsayd@gmail.com>
2025-11-12 00:10:36 +0200

    Initializing project structure

---
+++ src/com/restaurant/Main.java
@@ -0,0 +1,10 @@
+package com.restaurant;
+
+//import com.restaurant.ui.WorkflowController;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Starting Restaurant Ordering System...");
+        // Later this will call: new WorkflowController().start();
+    }
+}
